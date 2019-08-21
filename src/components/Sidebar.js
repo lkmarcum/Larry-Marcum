70a96a5c3b3e3045468ff15ccdaab777e3a47cc9
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-08-21 15:18:18 -0500

    updated icons, adjusted styles

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,25 +3,21 @@ import larry from "../larry.JPG";
 import github from "../github.png";
 import twitter from "../twitter.png";
 import email from "../email.png";
+import { Icon } from "semantic-ui-react";
+import "semantic-ui-css";
 
 const Sidebar = () => {
   return (
     <div className="side">
-      <img src={larry} alt="Larry Marcum" />
+      <img className="top-img" src={larry} alt="Larry Marcum" />
       <p>
         <strong>I am Larry</strong>, a Full-Stack Developer living in San
         Antonio, Texas.
       </p>
       <div className="link-container">
-        <a href="#">
-          <img src={github} alt="github" />
-        </a>
-        <a href="#">
-          <img src={twitter} alt="twitter" />
-        </a>
-        <a href="#">
-          <img className="email" src={email} alt="email" />
-        </a>
+        <Icon color="grey" name="github" />
+        <Icon color="grey" name="mail" />
+        <Icon color="grey" name="twitter" />
       </div>
     </div>
   );
